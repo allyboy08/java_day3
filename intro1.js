8efcd2707a26229a10a0cb62397dbfd410532c6b
allyboy08 <58805512+allyboy08@users.noreply.github.com>
2020-11-05 16:15:54 +0200

    Add files via upload

---
+++ intro1.js
@@ -0,0 +1,24 @@
+/*    Make a variable with a data of a string and check the type of this variable.
+
+    Reassign the value to a number and check the type again. Did it change?
+
+    Go ahead and create a variable with every data type you know of. Double check that you covered all of them by using the typeof check.*/
+
+let myname = "Bob";
+document.write(typeof myname);
+document.write("<br/>");
+let mynum = 8;
+document.write(typeof mynum);
+document.write("<br/>");
+/*let myund;
+document.write(typeof myund);*/
+
+let anything = null;
+document.write(typeof anything + "<br/>");
+
+let bobble = undefined;
+document.write(typeof bobble);
+document.write("<br/>");
+let is_greater_or_less = 3 > 5;
+document.write(typeof is_greater_or_less);
+document.write("<br/>");
